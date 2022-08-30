396feedbac1609fdde41d6cdad831c71e9b3a608
NGW <tnkngw29@gmail.com>
2022-08-31 06:30:47 +0900

    1.1 Change bookcase capacity to variable.

--- 1.1_Iterator/BookShelf.java
+++ 1.1_Iterator/BookShelf.java
@@ -1,24 +1,24 @@
+import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.List;
 
 public class BookShelf implements Iterable<Book> {
-    private Book[] books;
-    private int last = 0;
+    private List<Book> books;
 
-    public BookShelf(int maxsize) {
-        this.books = new Book[maxsize];
+    public BookShelf(int initialSize) {
+        this.books = new ArrayList<Book>(initialSize);
     }
 
     public Book getBookAt(int index) {
-        return books[index];
+        return books.get(index);
     }
 
     public void appendBook(Book book) {
-        this.books[last] = book;
-        last++;
+        books.add(book);
     }
 
     public int getLength() {
-        return last;
+        return books.size();
     }
 
     @Override
